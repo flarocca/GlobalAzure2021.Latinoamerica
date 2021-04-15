e22039f91fc80e0f1b8ba12622a7f3c9bb3bd042
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2021-04-15 20:01:31 -0300

    MVC integration implemented

--- GlobalAzure2021.Latinoamerica.MVC/Startup.cs
+++ GlobalAzure2021.Latinoamerica.MVC/Startup.cs
@@ -25,7 +25,10 @@ namespace GlobalAzure2021.Latinoamerica.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddMicrosoftIdentityUI();
+
+            services.AddMicrosoftIdentityWebAppAuthentication(Configuration, "AzureAdB2C");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +48,7 @@ namespace GlobalAzure2021.Latinoamerica.MVC
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
